e66772ee1e89fed9b39dc1b85d619b3f08d57b24
zuhdi-in <fzhd.03@gmail.com>
2022-09-25 22:54:59 +0700

    js learning assignment

---
+++ tugas7_js/js/script.js
@@ -0,0 +1,206 @@
+$("document").ready(function() {
+    $("li#ayam").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2016/11/29/05/32/rooster-1867562__340.jpg        ')",
+            "background-repeat": "no-repeat",
+            "background-size": "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#ayam").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/ayam.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#bebek").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2016/06/17/13/02/ducks-1463317__340.jpg')",
+            "background-repeat": "no-repeat",
+            "background-size": "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#bebek").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/bebek.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#merpati").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2020/09/12/18/18/collared-5566393__340.jpg')",
+            "background-repeat": "no-repeat",
+            "background-size": "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#merpati").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/merpati.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#gagak").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2017/03/21/18/46/raven-2162966__340.jpg')",
+            "background-repeat": "no-repeat",
+            "background-size": "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#gagak").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/gagak.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })  
+    $("li#sapi").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2016/10/04/23/52/cow-1715829_960_720.jpg     ')",
+            "background-repeat" : "no-repeat",
+            "background-size" : "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#sapi").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/sapi.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#kambing").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2014/09/24/15/15/billy-goat-459232__340.jpg')",
+            "background-repeat" : "no-repeat",
+            "background-size" : "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#kambing").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/kambing.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#babi").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2013/11/20/23/54/pig-214349__340.jpg')",
+            "background-repeat" : "no-repeat",
+            "background-size" : "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#babi").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/babi.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#kuda").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2017/06/21/15/08/horses-2427513__340.jpg')",
+            "background-repeat" : "no-repeat",
+            "background-size" : "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#kuda").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/kuda.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#harimau").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2016/11/29/10/07/tiger-1868911__340.jpg')",
+            "background-repeat" : "no-repeat",
+            "background-size" : "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#harimau").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/harimau.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#elang").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2016/10/19/13/42/eagle-1753002__340.jpg')",
+            "background-repeat" : "no-repeat",
+            "background-size" : "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#elang").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/elang.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#ular").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2014/08/15/21/40/snake-419043__340.jpg')",
+            "background-repeat" : "no-repeat",
+            "background-size" : "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#ular").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/ular.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+    $("li#serigala").click(function() {
+        $(this).css({
+            "background" : "url('https://cdn.pixabay.com/photo/2016/04/18/10/17/wolf-1336229__340.jpg')",
+            "background-repeat" : "no-repeat",
+            "background-size" : "cover",
+            "background-position" : "center",
+            "width" : "100px",
+            "height" : "100px"
+        })
+    })
+    $("li#serigala").click(function() {
+        $("<audio></audio>").attr({
+            'src': 'audio/serigala.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div")
+    })
+})
