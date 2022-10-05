2be22aa01b82622a9567362412af4ef7148079a4
zuhdi-in <73019174+zuhdi-in@users.noreply.github.com>
2022-10-06 00:05:16 +0700

    Update portofolio.php

---
+++ tugas6_php/portofolio.php
@@ -0,0 +1,22 @@
+<table align="center">
+    <thead>
+        <tr>
+            <th>Nama Project</th>
+            <th>Link Project</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Covid ID</td>
+            <td><a href="https://react-covid-zuhdi-in.vercel.app/">https://react-covid-zuhdi-in.vercel.app/</a></td>
+        </tr>
+        <tr>
+            <td>Movie Database</td>
+            <td><a href="https://movie-database-zuhdi-in.vercel.app/">https://movie-database-zuhdi-in.vercel.app/</a></td>
+        </tr>
+        <tr>
+            <td>Web Porto</td>
+            <td><a href="https://movie-database-zuhdi-in.vercel.app/">https://movie-database-zuhdi-in.vercel.app/</a></td>
+        </tr>
+    </tbody>
+</table>
