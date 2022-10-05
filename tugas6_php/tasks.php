c116694a6b27ff7d0b72a99a3736e5a81cc3010c
zuhdi-in <73019174+zuhdi-in@users.noreply.github.com>
2022-10-06 00:04:49 +0700

    Update tasks.php

--- tugas6_php/tasks.php
+++ tugas6_php/tasks.php
@@ -1,4 +1,4 @@
-<table>
+<table align="center">
     <thead>
         <tr>
             <th>Nama Kelas</th>
@@ -6,7 +6,17 @@
         </tr>
     </thead>
     <tbody>
-        <td>Kelas Web Design</td>
-        <td>https://github.com/zuhdi-in/nfcom/tree/webDesign_class</td>
+        <tr>
+            <td>Kelas Web Design</td>
+            <td><a href="https://github.com/zuhdi-in/nfcom/tree/webDesign_class">https://github.com/zuhdi-in/nfcom/tree/webDesign_class</a></td>
+        </tr>
+        <tr>
+            <td>Kelas Javascript</td>
+            <td><a href="https://github.com/zuhdi-in/nfcom/tree/javascript_class">https://github.com/zuhdi-in/nfcom/tree/javascript_class</a></td>
+        </tr>
+        <tr>
+            <td>Kelas PHP</td>
+            <td><a href="https://github.com/zuhdi-in/nfcom/tree/php_class">https://github.com/zuhdi-in/nfcom/tree/php_class</a></td>
+        </tr>
     </tbody>
 </table>
